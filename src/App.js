7d260aa671d8a23da73629dc70580ee4f43a6b3b
miss-gif <svx327@naver.com>
2024-07-01 13:10:58 +0900

    Docs : axios ts 버전

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useEffect } from "react";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useEffect } from "react";
 import {
   deleteTodo,
   getTodo,
@@ -10,16 +11,17 @@ import {
 
 const App = () => {
   useEffect(() => {
-    getTodo(3);
-    getTodos();
-    const todo = {
-      title: "안녕하세요. 오늘 할 일입니다.",
-      completed: false,
-    };
-    postTodo(todo);
-    putTodo(3, todo);
-    patchTodo(5);
-    deleteTodo();
+    const result = getTodo(3);
+    console.log(result);
+    // getTodos();
+    // const todo = {
+    //   title: "안녕하세요. 오늘 할일입니다.",
+    //   completed: false,
+    // };
+    // postTodo(todo);
+    // putTodo(3, todo);
+    // patchTodo(5, todo);
+    // deleteTodo(10);
   }, []);
   return <div></div>;
 };
